b710d9df5b2990e6fb4c6cca8c1bcc0566d8f2fb
Adi Sharma <114644930+asivp@users.noreply.github.com>
2024-07-19 19:26:15 +0530

    Adding files in src folder

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Adaptabletestgrid from './Adaptabletestgrid';
+
+function App() {
+  return (
+    <div className="App">
+      <Adaptabletestgrid />
+    </div>
+  );
+}
+
+export default App;
